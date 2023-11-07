a65c9479115a5a7e73d145195273f8a07ca04939
sevans <sevans@highdesertmilk.com>
2023-11-07 10:38:47 -0700

    Video 6

---
+++ dojo-helpdesk/app/tickets/TicketList.jsx
@@ -0,0 +1,33 @@
+// This component will fetch the ticket data and output it inside the template
+
+// fetch logic 
+async function getTickets() { //got this data by npm installing json-server
+    const res = await fetch('http://localhost:4000/tickets', {
+        next: {// this is set to the amount of time that NextJS should wait since last page visit before revalidating the cached data again.
+            revalidate: 0 // use 0 to opt out of using cache
+        }
+    }) 
+
+    return res.json() // this is returned promise
+}
+
+export default async function TicketList() {
+    const tickets = await getTickets()
+
+    return (
+       <>
+            {tickets.map((ticket) => (
+                <div key={ticket.id} className="card my-5">
+                    <h3>{ticket.title}</h3>
+                    <p>{ticket.body.slice(0, 200)}...</p>
+                    <div className={`pill ${ticket.priority}`}>
+                        {ticket.priority} priority
+                    </div>
+                </div>
+            ))}
+            {tickets.length === 0 && (
+                <p className="text-center">There are no open tickets, yay!</p>
+            )}
+       </>
+    )
+}
