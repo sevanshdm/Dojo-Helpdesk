9eac7854ca2cde93d1fed87db51e1395af8d89e1
sevans <sevans@highdesertmilk.com>
2023-11-08 08:53:15 -0700

    Video 11 - Client Form Component

--- dojo-helpdesk/app/loading.jsx
+++ dojo-helpdesk/app/loading.jsx
@@ -1,8 +1,8 @@
 // this file creates a loading component to be used with suspense comoponents which create suspense boundaries
 // these are found on page.jsx files. Ulitmately results in loading screens
 
-
-export default function Loading() {
+//an example of when this file is used can be found in tickets folder, page.jsx line 16
+export default function Loading() { 
     return (
         <main className="text-center">
             <h2 className="text-primary">Loading...</h2>
